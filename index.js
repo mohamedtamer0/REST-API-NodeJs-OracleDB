27f1806cb16837373a514bfd71dec3d53c28d822
Mahmoud R. Alnakeeb <59762294+mahmoudalnkeeb@users.noreply.github.com>
2022-11-09 15:33:18 +0200

    release connection after finish with connection.close()

--- index.js
+++ index.js
@@ -3,23 +3,28 @@ const oracleDb = require('oracledb');
 oracleDb.outFormat = oracleDb.OUT_FORMAT_OBJECT;
 
 async function fun() {
-    let con;
+  let con;
+  try {
+    con = await oracleDb.getConnection({
+      user: '..',
+      password: '..',
+      connectionString: 'host:port/serviceName',
+    });
 
-    try {
-        con = await oracleDb.getConnection({
-            user: "..",
-            password: "..",
-            connectionString: "host:port/serviceName"
-        });
-
-        const data = await con.execute(
-            `select * from write Your table name`,
-        );
-
-        console.log(data.rows);
-    } catch (err) {
-        console.log(err);
+    const data = await con.execute(`select * from write Your table name`);
+    console.log(data.rows);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    if (con) {
+      try {
+        await con.close();
+      } catch (e) {
+        console.error(`error while close connection: 
+${e} `);
+      }
     }
+  }
 }
 
 fun();
