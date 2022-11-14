d42e9c05c4efd255ab045a4d85e110214004e7bb
mohamed.tamer <mt4452242@gmail.com>
2022-11-14 20:13:34 +0200

    update

--- index.js
+++ index.js
@@ -1,25 +1,78 @@
-const oracleDb = require('oracledb');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+const oracledb = require('oracledb');
+oracledb.autoCommit = true; //Commita queries
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
-oracleDb.outFormat = oracleDb.OUT_FORMAT_OBJECT;
+const dbConfig = {
+    user: "..",
+    password: "..",
+    connectionString: "host:port/serviceName",
+    poolMin: 10,
+    poolMax: 10,
+    poolIncrement: 0
+};
 
-async function fun() {
-    let con;
 
-    try {
-        con = await oracleDb.getConnection({
-            user: "..",
-            password: "..",
-            connectionString: "host:port/serviceName"
+app.get('/find', (req, res) => {
+    let users = new Array();
+    let connection;
+    oracledb.getConnection(dbConfig)
+        .then((c) => {
+            connection = c;
+            return connection.execute("select * from write Your table name");
+        })
+        .then((result) => {
+            result.rows.forEach((elemento) => {
+                let user = new Object();
+                user.EMPID = elemento[0];
+                user.USERNAME = elemento[1];
+                user.PASSWORD = elemento[2];
+                users.push(user);
+            });
+            res.status(200).json(users);
+        }).then(() => {
+            if (connection) {
+                connection.close();
+            }
+        }).catch((error) => {
+            res.status(500).json({ message: error.message || "Some error occurred!" });
         });
+});
 
-        const data = await con.execute(
-            `select * from write Your table name`,
-        );
 
-        console.log(data.rows);
-    } catch (err) {
-        console.log(err);
-    }
-}
 
-fun();
+app.get('/find/:userId', (req, res) => {
+    let connection;
+    let user = new Object();
+    oracledb.getConnection(dbConfig)
+        .then((c) => {
+            connection = c;
+            return connection.execute("select * from write Your table name WHERE EMPID = :EMPID ", {
+                EMPID: req.params.userId
+            });
+        })
+        .then((result) => {
+            result.rows.forEach((elemento) => {
+                user.EMPID = elemento[0];
+                user.USERNAME = elemento[1];
+                user.PASSWORD = elemento[2];
+            });
+            res.status(200).json(user);
+        }).then(() => {
+            if (connection) {
+                connection.close();
+            }
+        }).catch((error) => {
+            res.status(500).json({ message: error.message || "Some error occurred!" });
+        });
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`http://localhostt:${PORT}`);
+});
